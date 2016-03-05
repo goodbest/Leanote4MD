8bdfb1c0fdc0d7166e2073ef9f5656d52da275fc
Svtter <svtter@qq.com>
2016-03-05 23:50:16 +0800

    Update Leanote4MD.py
    修改导出程序，输出Error但不终止导出文件

--- Leanote4MD.py
+++ Leanote4MD.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #encoding: utf8
-# 
+#
 # author: goodbest <lovegoodbest@gmail.com>
 # github: github.com/goodbest
 
@@ -23,7 +23,7 @@ def is_ok(myjson):
       json_object = json.loads(myjson)
     except ValueError, e:
       return False
-    
+
     if 'Ok' in json_object:
         if json_object['Ok']:
             return True
@@ -40,10 +40,10 @@ def req_get(url, param = '', type = 'json', token = True):
             param.update({'token': leanote_token})
         else:
             param={'token': leanote_token}
-            
+
     s = requests.Session()
     if leanote_host.startswith('https'):
-        s.mount('https://', SSLAdapter(ssl.PROTOCOL_TLSv1))        
+        s.mount('https://', SSLAdapter(ssl.PROTOCOL_TLSv1))
     r = s.get(leanote_host + '/api/' + url, params = param)
     if r.status_code == requests.codes.ok:
         if type=='json':
@@ -58,7 +58,7 @@ def req_get(url, param = '', type = 'json', token = True):
         elif type=='image':
             i = Image.open(StringIO(r.content))
             return i
-            
+
     else:
         print '[Err] connect to url %s fail, error code %d ' %(r.url, r.status_cde)
         return None
@@ -70,7 +70,7 @@ def req_post(url, param = '', type = 'json', token = True):
             param.update({'token': leanote_token})
         else:
             param={'token': leanote_token}
-    
+
     s = requests.Session()
     if leanote_host.startswith('https'):
         s.mount('https://', SSLAdapter(ssl.PROTOCOL_TLSv1))
@@ -85,7 +85,7 @@ def req_post(url, param = '', type = 'json', token = True):
             else:
                 print '[Err] requests to url %s fail' %(r.url)
                 return None
-                
+
     else:
         print '[Err] connect to url %s fail, error code %d ' %(r.url, r.status_cde)
         return None
@@ -183,7 +183,7 @@ def readFromFile(filename):
                 else:
                     file_meta += line
                     #print meta
-        
+
         if not meta_flag:
             file_content = file_meta
             file_meta = ''
@@ -201,7 +201,7 @@ def saveToFile(notes, noteBooks, path = '.'):
             filename = note['NoteId']
         else:
             filename = note['Title']
-        
+
         if filename in unique_noteTitle:
             filename='%s_%s' %(filename, note['NoteId'])
         else:
@@ -211,44 +211,50 @@ def saveToFile(notes, noteBooks, path = '.'):
             filename += '.md'
         else:
             filename += '.txt'
-        with open(path + '/' + filename, 'w') as file:
-            print 'write file: %s' %filename
-            file.write('title: %s\n' %note['Title'].encode('utf-8'))
-        
-            date = dateutil.parser.parse(note['CreatedTime'])
-            file.write('date: %s\n' %datetime.strftime(date.astimezone(local_zone), '%Y/%m/%d %H:%M:%S'))
-        
-            date = dateutil.parser.parse(note['UpdatedTime'])
-            file.write('updated: %s\n' %datetime.strftime(date.astimezone(local_zone), '%Y/%m/%d %H:%M:%S'))
-        
-            if note['Tags']:
-                if len(note['Tags']) == 1:
-                    if note['Tags'][0]:
-                        file.write('tags:\n')
-                        for tag in note['Tags']:
-                            file.write('- %s\n' %tag.encode('utf-8'))
-        
-            category = []
-            current_notebook = note['NotebookId']
-            category.append(noteBooks[current_notebook]['Title'])
-            while noteBooks[current_notebook]['ParentNotebookId'] != '':
-                category.append(noteBooks[noteBooks[current_notebook]['ParentNotebookId']]['Title'])
-                current_notebook = noteBooks[current_notebook]['ParentNotebookId']
-            file.write('categories:\n')
-            category.reverse()
-            for cat in category:
-                file.write('- %s\n' %cat.encode('utf-8'))            
-        
-            file.write('---\n')
-            file.write('%s' %note['Content'].encode('utf-8'))
-        file.close()
-        if note['Files']:
-            if len(note['Files']) > 0:
-                for attach in note['Files']:
-                    if not attach['IsAttach']:
-                        i = getImage(attach['FileId'])
-                        print 'saving its image: %s.%s' %(attach['FileId'], i.format)
-                        i.save(attach['FileId'] + '.' + i.format)
+        try:
+            with open(path + '/' + filename, 'w') as file:
+                print 'write file: %s' %filename
+                file.write('title: %s\n' %note['Title'].encode('utf-8'))
+
+
+                date = dateutil.parser.parse(note['CreatedTime'])
+                file.write('date: %s\n' %datetime.strftime(date.astimezone(local_zone), '%Y/%m/%d %H:%M:%S'))
+
+                date = dateutil.parser.parse(note['UpdatedTime'])
+                file.write('updated: %s\n' %datetime.strftime(date.astimezone(local_zone), '%Y/%m/%d %H:%M:%S'))
+
+                if note['Tags']:
+                    if len(note['Tags']) == 1:
+                        if note['Tags'][0]:
+                            file.write('tags:\n')
+                            for tag in note['Tags']:
+                                file.write('- %s\n' %tag.encode('utf-8'))
+
+                category = []
+                current_notebook = note['NotebookId']
+                category.append(noteBooks[current_notebook]['Title'])
+                while noteBooks[current_notebook]['ParentNotebookId'] != '':
+                    category.append(noteBooks[noteBooks[current_notebook]['ParentNotebookId']]['Title'])
+                    current_notebook = noteBooks[current_notebook]['ParentNotebookId']
+                file.write('categories:\n')
+                category.reverse()
+                for cat in category:
+                    file.write('- %s\n' %cat.encode('utf-8'))
+
+                file.write('---\n')
+                file.write('%s' %note['Content'].encode('utf-8'))
+
+            file.close()
+            if note['Files']:
+                if len(note['Files']) > 0:
+                    for attach in note['Files']:
+                        if not attach['IsAttach']:
+                            i = getImage(attach['FileId'])
+                            print 'saving its image: %s.%s' %(attach['FileId'], i.format)
+                            i.save(attach['FileId'] + '.' + i.format)
+
+        except:
+            print "error: ", filename
 
 
 def LeanoteExportToMD(path = '.'):
@@ -283,15 +289,15 @@ def LeanoteImportFromMD(path = '.'):
         importedNotebookTitleMapID['import'] = ret['NotebookId']
 
     for filename in filelist:
-        content, meta = readFromFile(path + '/' + filename)  
-    
+        content, meta = readFromFile(path + '/' + filename)
+
         parentTitle='import'
         currentTitle=''
         if not meta.get('categories'):
             categories=['import']
         else:
             categories= meta.get('categories')
-        for cat in categories:    
+        for cat in categories:
             currentTitle=cat
             if currentTitle in importedNotebookTitleMapID.keys():
                 parentTitle=currentTitle
@@ -299,7 +305,7 @@ def LeanoteImportFromMD(path = '.'):
                 ret = addNotebook(title = currentTitle, parentId = importedNotebookTitleMapID[parentTitle])
                 importedNotebookTitleMapID[currentTitle] = ret['NotebookId']
                 parentTitle=currentTitle
-        
+
         if not meta.get('title'):
             meta['title'] = filename.replace('.md','').replace('.txt','')
         importedNote = addNote(NotebookId=importedNotebookTitleMapID[currentTitle], Title=meta.get('title'), Content=content, Tags=meta.get('tags', []), Abstract='')
@@ -311,7 +317,7 @@ def LeanoteImportFromMD(path = '.'):
 
 if __name__ == '__main__':
     choice = raw_input("Enter your choice: (import or export) ")
-        
+
     leanote_host = raw_input("Enter your host: (default is http://leanote.com) ")
     if not leanote_host:
         leanote_host = 'http://leanote.com'
@@ -329,23 +335,23 @@ if __name__ == '__main__':
     print 'Connecting to %s' %leanote_host
     leanote_token = login(leanote_email, leanote_password)
     local_zone=tz.tzlocal()
-    
-    if choice == 'import':        
+
+    if choice == 'import':
         LeanoteImportFromMD(path)
         exit()
 
     elif choice == 'export':
         LeanoteExportToMD(path)
         exit()
-    
+
     else:
         print 'command format: \npython Leanote4MD.py import\npython Leanote4MD.py export'
 
 
 
-    
-
-    
 
 
-    
+
+
+
+
